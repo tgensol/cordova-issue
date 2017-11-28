8a5a2509524465b3de05e7526ed4c633dc3cc3d7
Thibaut Gensollen <thibaut.gensollen@ens-cachan.fr>
2017-11-28 10:55:53 +0100

    still issue

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,66 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "ionic-plugin-keyboard.keyboard",
+    "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
+    "pluginId": "ionic-plugin-keyboard",
+    "clobbers": [
+      "cordova.plugins.Keyboard"
+    ],
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+    "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+    "pluginId": "cordova-plugin-ionic-webview",
+    "clobbers": [
+      "cordova.exec"
+    ]
+  },
+  {
+    "id": "cordova-universal-clipboard.Clipboard",
+    "file": "plugins/cordova-universal-clipboard/www/clipboard.js",
+    "pluginId": "cordova-universal-clipboard",
+    "clobbers": [
+      "cordova.plugins.clipboard"
+    ]
+  },
+  {
+    "id": "cordova-plugin-google-analytics.UniversalAnalytics",
+    "file": "plugins/cordova-plugin-google-analytics/www/analytics.js",
+    "pluginId": "cordova-plugin-google-analytics",
+    "clobbers": [
+      "analytics",
+      "ga"
+    ]
+  },
+  {
+    "id": "cordova-plugin-facebook4.FacebookConnectPlugin",
+    "file": "plugins/cordova-plugin-facebook4/www/facebook-native.js",
+    "pluginId": "cordova-plugin-facebook4",
+    "clobbers": [
+      "facebookConnectPlugin"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "ionic-plugin-keyboard": "2.2.1",
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-splashscreen": "4.1.0",
+  "cordova-plugin-ionic-webview": "1.1.16",
+  "cordova-universal-clipboard": "0.1.0",
+  "cordova-plugin-google-analytics": "1.8.3",
+  "cordova-plugin-facebook4": "1.9.1"
+};
+// BOTTOM OF METADATA
+});
